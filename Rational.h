24f7a91ae6468d6acc8815772db1fd53ba59caf4
maksimv0202 <maksimv0202@gmail.com>
2022-01-12 22:26:22 +1000

    class_rational_arithmetic_and_iostream_operators

--- Rational.h
+++ Rational.h
@@ -7,23 +7,52 @@ private:
 public:
     Rational(): numerator(1), denumerator(1) {}
 
-    Rational(int num, int denum): numerator(num), denumerator(denum) {}
-    
-    Rational operator+=(const Rational& rhs);
+    Rational(int num, int denum): numerator(num), denumerator(denum) {} 
 
-    Rational operator-=(const Rational& rhs);
+    Rational operator=(const Rational& rhs) {
+        this->numerator     = rhs.numerator;
+        this->denumerator   = rhs.denumerator;
+    }
 
-    Rational operator*=(const Rational& rhs);
+    Rational operator+=(const Rational& rhs) {
+        this->numerator     = this->numerator * rhs.denumerator + this->denumerator * rhs.numerator;
+        this->denumerator  *= rhs.denumerator;
+        return *this;
+    }
 
-    Rational operator/=(const Rational& rhs);
+    Rational operator-=(const Rational& rhs) {
+        this->numerator     = this->numerator * rhs.denumerator - this->denumerator * rhs.numerator;
+        this->denumerator  *= rhs.denumerator;
+        return *this;
+    }
 
-    Rational operator+(const Rational& rhs);
+    Rational operator*=(const Rational& rhs) {
+        this->numerator     *= rhs.numerator;
+        this->denumerator   *= rhs.denumerator;
+        return *this;
+    }
 
-    Rational operator-(const Rational& rhs);
+    Rational operator/=(const Rational& rhs) {
+        this->numerator     *= rhs.denumerator;
+        this->denumerator   *= rhs.numerator;
+        return *this;
+    }
 
-    Rational operator*(const Rational& rhs);
+    Rational operator+(const Rational& rhs) {
+        return *this += rhs;
+    }
 
-    Rational operator/(const Rational& rhs);
+    Rational operator-(const Rational& rhs) {
+        return *this -= rhs;
+    }
+
+    Rational operator*(const Rational& rhs) {
+        return *this *= rhs;
+    }
+
+    Rational operator/(const Rational& rhs) {
+        return *this /= rhs;
+    }
 
     inline bool operator==(const Rational& rhs);
 
@@ -37,9 +66,18 @@ public:
 
     inline bool operator>=(const Rational& rhs);
 
-    std::ostream& operator<<(std::ostream& out);
+    friend std::ostream& operator<<(std::ostream& out, const Rational& num);
 
-    std::istream& operator>>(std::istream& in);
+    friend std::istream& operator>>(std::istream& in, Rational& num);
 
     ~Rational() = default;
 };
+
+std::ostream& operator<<(std::ostream& out, const Rational& num) {
+   return out << num.numerator << '/' << num.denumerator;
+}
+
+std::istream& operator>>(std::istream& in, Rational& num) {
+    return in >> num.numerator >> num.denumerator;
+}
+
