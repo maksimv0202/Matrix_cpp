db4ec8b497c54f4a297a81ffdc025e1dabb62921
maksimv0202 <maksimv0202@gmail.com>
2022-01-16 00:48:39 +1000

    class_matrix_definition

--- Matrix.h
+++ Matrix.h
@@ -2,5 +2,47 @@
 
 #include <iostream>
 
-template <typename T, size_t M, size_t N, typename Alloc = std::allocator<T>>
-class Matrix;
+template <typename T = int, size_t M = 0, size_t N = 0, typename Alloc = std::allocator<T>>
+class Matrix {
+private:
+    T matrix[M][N];
+public:
+    Matrix() {}
+    
+    Matrix(std::initializer_list<T> lst) {}
+
+    Matrix(const T& fill) {}
+    
+    Matrix(const Matrix<T, M, N>& matrix) {}
+
+    Matrix(Matrix<T, M, N>&& matrix) {}
+
+    Matrix& operator=(const Matrix<T, M, N>& matrix) {}
+
+    Matrix& operator=(Matrix<T, M, N>&& matrix) {}
+    
+    Matrix<T, M, N>& operator+=(const Matrix<T, M, N>& r_matrix) {}
+    
+    Matrix<T, M, N>& operator-=(const Matrix<T, M, N>& r_matrix) {}
+    
+    template <size_t K>
+    Matrix<T, M, K>& operator*=(const Matrix<T, N, K>& r_matrix) {}
+    
+    Matrix<T, M, N>& operator+(const Matrix<T, M, N>& r_matrix) {}
+    
+    Matrix<T, M, N>& operator-(const Matrix<T, M, N>& r_matrix) {}
+   
+    template <size_t K>
+    Matrix<T, M, K>& operator*(const Matrix<T, N, K>& r_matrix) {}
+    
+    T __det() {}
+
+    Matrix<T, M, M>& __transponse() {}
+    
+    Matrix<T, M, M> __inverse() {}
+    
+    T __tr() {}
+
+    ~Matrix() {}
+};
+
