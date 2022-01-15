643de132e495f73662058b9e984983c063370f8f
maksimv0202 <maksimv0202@gmail.com>
2022-01-15 22:00:29 +1000

    class_complex_implementation_of_functions

--- Complex.h
+++ Complex.h
@@ -1,4 +1,8 @@
+#pragma once
+
+#include <cmath>
 #include <iostream>
+#include "Rational.h"
 
 template <typename T>
 class Complex {
@@ -6,51 +10,166 @@ private:
     T re;
     T im;
 public:
-    Complex(): re(0), im(0) {}
+    Complex(): re(0.0), re(0.0) {}
+    
+    Complex(const T& re, const T& im): re(re), im(im) {}
+    
+    Complex(const T& re): re(re), im(0.0) {}
 
-    Complex(T re): re(re), im(0) {}
+    Complex(const Complex<T>& z): re(z.re), im(z.im) {}
 
-    Complex& operator=(const Complex& z);
+    Complex(Complex<T>&& z) noexcept: re(std::move(z.re)), im(std::move(z.im)) {}
 
-    Complex& operator+=(const Complex& rhs);
+    Complex<T> conjugate() {
+        Complex<T> copy = *this;
+        copy.im         = -im;
+        return copy;
+    }
 
-    Complex& operator-=(const Complex& rhs);
+    Complex<T> conjugate() const noexcept {
+        Complex<T> copy = *this;
+        copy.im = -copy.im;
+        return copy;
+    }
 
-    Complex& operator*=(const Complex& rhs);
+    double abs() noexcept {
+        double abs = sqrt(re * re + im * im);
+        return abs;
+    }
 
-    Complex& operator/=(const Complex& rhs);
+    double abs() const noexcept {
+        const double abs = sqrt(re * re + im * im);
+        return abs;
+    }
 
-    Complex& operator+(const Complex& rhs);
+    double abs_sqrt() noexcept {
+        return (*this).abs() * (*this).abs();
+    }
 
-    Complex& operator-(const Complex& rhs);
+    double abs_sqrt() const noexcept {
+        return (*this).abs() * (*this).abs();
+    }
 
-    Complex& operator*(const Complex& rhs);
+    Complex<T>& operator=(const Complex<T>& z) {
+        re = z.re;
+        im = z.im;
+        return *this;
+    }
 
-    Complex& operator/(const Complex& rhs);
+    Complex<T>& operator=(Complex<T>&& z) noexcept {
+        re = std::move(z.re);
+        im = std::move(z.im);
+        return *this;
+    }
 
-    Complex& operator++();
+    Complex<T>& operator+=(const Complex<T>& z) {
+        re += z.re;
+        im += z.im;
+        return *this;
+    }
 
-    Complex operator++(int);
+    Complex<T>& operator-=(const Complex<T>& z) {
+        re -= z.re;
+        im -= z.im;
+        return *this;
+    }
 
-    Complex& operator--();
+    Complex<T>& operator*=(const Complex<T>& z) {
+        Complex<T> copy = *this;
+        re = copy.re * z.re - copy.im * z.im;
+        im = copy.re * z.im + z.re * copy.im;
+        return *this;
+    }
 
-    Complex operator--(int);
+    Complex<T>& operator/=(const Complex<T>& z) {
+        *this   *= z.conjugate();
+        re      /= z.abs_sqrt();
+        im      /= z.abs_sqrt();
+        return *this; 
+    }
 
-    inline bool operator==(const Complex& rhs);
+    Complex<T> operator+(const Complex<T>& z) {
+        Complex<T> copy = *this;
+        copy            += z;
+        return copy;
+    }
 
-    inline bool operator!=(const Complex& rhs);
+    Complex<T> operator-(const Complex<T>& z) {
+        Complex<T> copy = *this;
+        copy            -= z;
+        return copy;
+    }
 
-    inline bool operator<(const Complex& rhs);
+    Complex<T> operator*(const Complex<T>& z) {
+        Complex<T> copy = *this;
+        copy            *= z;
+        return copy;
+    }
 
-    inline bool operator>(const Complex& rhs);
+    Complex<T> operator/(const Complex<T>& z) {
+        Complex<T> copy = *this;
+        copy            /= z;
+        return copy;
+    }
 
-    inline bool operator<=(const Complex& rhs);
+    Complex<T>& operator++() {
+        re += 1.0;
+        im += 1.0;
+        return *this;
+    }
 
-    inline bool operator>=(const Complex& rhs);
+    Complex<T> operator++(int) {
+        Complex<T> copy = *this;
+        ++(*this);
+        return copy; 
+    }
 
-    friend std::ostream& operator<<(std::ostream& out, const Complex& z);
+    Complex<T>& operator--() {
+        re -= 1.0;
+        im -= 1.0;
+        return *this;
+    }
 
-    friend std::istream& operator>>(std::istream& in, Complex& z);
+    Complex<T> operator--(int) {
+        Complex<T> copy = *this;
+        --(*this);
+        return copy;
+    }
+
+    inline bool operator==(const Complex<T>& z) {
+        return !(re == z.re + im == z.im);
+    }
+
+    inline bool operator!=(const Complex<T>& z) {
+        return !(*this == z);
+    }
+
+    inline bool operator<(const Complex<T>& rhs) = delete;
+
+    inline bool operator>(const Complex<T>& rhs) = delete;
+
+    inline bool operator<=(const Complex<T>& rhs) = delete;
+
+    inline bool operator>=(const Complex<T>& rhs) = delete;
+    
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& out, const Complex<U>& z);
+
+    template <typename U>
+    friend std::istream& operator>>(std::istream& in, Complex<U>& z);
 
     ~Complex() = default;
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Complex<T>& z) {
+    out << '(' << z.re << ',' << ' ' << z.im << ')';
+    return out;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& in, Complex<T>& z) {
+    in >> z.re >> z.im;
+    return in;
+}
+
